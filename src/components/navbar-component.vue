bec2471a2d31293af6724ea9574c494f3896141e
SyedAliHossini <salihoosainione@gmail.com>
2019-04-08 09:17:56 +0430

    add navbar

---
+++ src/components/navbar-component.vue
@@ -0,0 +1,30 @@
+<template>
+    <b-container fluid>
+        <b-navbar toggleable="lg" type="dark" variant="info">
+            <b-container>
+                <b-navbar-brand href="#">NavBar</b-navbar-brand>
+                <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+                <b-collapse id="nav-collapse" is-nav>
+                    <b-navbar-nav>
+                        <b-nav-item-dropdown text="Lang" right>
+                            <b-dropdown-item href="#">EN</b-dropdown-item>
+                            <b-dropdown-item href="#">ES</b-dropdown-item>
+                            <b-dropdown-item href="#">RU</b-dropdown-item>
+                            <b-dropdown-item href="#">FA</b-dropdown-item>
+                        </b-nav-item-dropdown>
+                    </b-navbar-nav>
+                </b-collapse>
+            </b-container>
+        </b-navbar>
+    </b-container>
+</template>
+
+<script>
+    export default {
+        name: "navbar-component"
+    }
+</script>
+
+<style scoped>
+
+</style>
